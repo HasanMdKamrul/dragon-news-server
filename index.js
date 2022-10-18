6ec91349c045a48bb0e52b7c89d963e5053ecb9a
HasanMdKamrul <kamrulhasan@iut-dhaka.edu>
2022-10-18 09:19:35 +0200

    git ignore and cors added

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const port = process.env.PORT || 15000;
 const categories = require("./data/categories.json");
+
+// ** cors solving issues -> cors is a middleware
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Dragon News Server");
