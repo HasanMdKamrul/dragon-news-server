1f4d35ac8f30d00d273af5a40a4e6e2f3849b6d7
HasanMdKamrul <kamrulhasan@iut-dhaka.edu>
2022-10-19 19:11:54 +0200

    Individual news and category wise newses api endpoints created

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const cors = require("cors");
 const port = process.env.PORT || 15000;
 const categories = require("./data/categories.json");
+const newsData = require("./data/news.json");
 
 // ** cors solving issues -> cors is a middleware
 app.use(cors());
@@ -11,10 +12,34 @@ app.get("/", (req, res) => {
   res.send("Dragon News Server");
 });
 
-// ** categories api
+// ** categories api load
 
 app.get("/categories-news", (req, res) => {
   res.send(categories);
+});
+
+// ** Individual news load
+
+app.get("/news/:id", (req, res) => {
+  const id = req.params.id;
+
+  const individualNews = newsData.find((news) => news._id === id);
+
+  res.send(individualNews);
+});
+
+// ** Categorywise news load
+
+app.get("/category/:id", (req, res) => {
+  const id = req.params.id;
+
+  if (id === "08") {
+    res.send(newsData);
+  } else {
+    const categoryNews = newsData.filter((news) => news.category_id === id);
+
+    res.send(categoryNews);
+  }
 });
 
 app.listen(port, () =>
